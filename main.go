f5aaea484365ed7d531929acd05fb7982a255656
Dharmit <dharmitshah009@gmail.com>
2023-02-16 01:11:40 +0530

    PLAIN CANVAS: NO CODE UPTIL NOW

--- main.go
+++ main.go
@@ -1,16 +1,7 @@
 package main
 
-import (
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
-)
+import "fmt"
 
 func main() {
-	myApp := app.New()
-	myWindow := myApp.NewWindow("### GOPASS ###")
-	myLabel := widget.NewLabel("Hello, World")
-	myContainer := container.NewCenter(myLabel)
-	myWindow.SetContent(myContainer)
-	myWindow.ShowAndRun()
+	fmt.Println("Main.go")
 }
