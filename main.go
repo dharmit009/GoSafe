1c1a6772528272d2fddda7863e2a568aed89e302
Dharmit <dharmitshah009@gmail.com>
2023-03-05 12:35:37 +0530

    Updated: main.go --> Blank Canvas & Now Starting with UI

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
-	fmt.Println("### Gopass ###")
+	// create a new Fyne app
+	fmt.Println("HELLO")
+	a := app.New()
+
+	// create a new window
+	w := a.NewWindow("My Window")
+
+	// create a label widget
+	label := widget.NewLabel("Hello, World!")
+
+	// set the content of the window to the label widget
+	w.SetContent(label)
+
+	// show the window
+	w.ShowAndRun()
 }
